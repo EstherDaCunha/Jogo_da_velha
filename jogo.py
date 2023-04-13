b1c1096dd5cf50984a12c5382e1ffeef22d9e4da
EstherDaCunha <128049330+EstherDaCunha@users.noreply.github.com>
2023-04-13 16:29:39 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,90 +1,118 @@
-velha = [[0, 0, 0],
-         [0, 0, 0],
-         [0, 0, 0]]
+class jogo_velha:
+    def __init__(self, mesa=[]): #Parametro opcional
+        """
+        Método construtor
+        :param mesa: O parametro mesa é igual a matriz do tabuleiro do jogo da velha.
+        """
+        self.velha = mesa
 
-def menu():
-    """
-    Pergunta ao jogador se ele deseja jogar ou sair, sendo 0 a opção de saída
-    e a opção 1 como iniciar jogo.
-    :return: None
-    """
-    iniciar = 1
-    while iniciar:
-        iniciar = int(input("0. Sair \n"+"1. Jogar \n"))
-        if iniciar:
-            jogo()
-        else:
-            print("Saindo...")
-
-def jogo():
-    """
-    Define as posições das jogadas (linhas e colunas) e identifica o ganhador.
-    :return:None
-    """
-    jogada = 0 #Número de jogadas, +1 a cada rodada
-
-    while ganhador() == 0:
-        print("Jogador", jogada % 2 + 1) #Jogada % 2 = 0 e 1 alternando + 1 = 1 e 2 alternando = jogador 1 ou 2
-        tabuleiro()
-        linha = int(input("Linha: "))
-        coluna = int(input("Coluna: "))
-        # Subtrai 1 para que o usuário possa usar as posições 1, 2 e 3
-        if velha [linha-1][coluna-1] == 0:
-            if(jogada % 2+1) == 1:
-                velha[linha-1][coluna-1] = 1
+    def iniciar(self):
+        """
+        Pergunta ao jogador se ele deseja jogar ou sair, sendo 0 a opção de saída
+        e a opção 1 como iniciar jogo.
+        :return: None
+        """
+        iniciar = 1
+        while iniciar:
+            iniciar = int(input("0. Sair \n"+"1. Jogar \n"))
+            if iniciar:
+                self.jogo()
             else:
-                velha[linha-1][coluna-1] = -1
-        else:
-            print("Não está vazio")
-            jogada -= 1
+                print("Saindo...")
+    @property #Getter
+    def mesa(self):
+        """
+        Puxa a variavel mesa.
+        :return:Matriz
+        """
+        return self.mesa
 
-        if ganhador():
-            print("Jogador", jogada % 2 + 1, "Ganhou após", jogada+1, "rodadas")
-        jogada += 1
+    @mesa.setter #Setter
+    def mesa(self, value):
+        """
+        Coloca a variavel.
+        :param value:value
+        :return:velha
+        """
+        self.velha = value
 
-        if jogada > 8:
-            print("Velha")
-            break
+    def jogo(self):
+        """
+        Define as posições das jogadas (linhas e colunas) e identifica o ganhador.
+        :return:None
+        """
+        jogada = 0 #Número de jogadas, +1 a cada rodada
+        velha_get = mesa
 
-def ganhador():
-    """
-    Checa se alguma linha, coluna ou diagonal está completa, se sim, apresenta o resultado, caso não,
-    as rodadas continuam
-    :return: Int
-    """
-    #Checar linhas
-    for i in range(3):
-        soma = velha[i][0]+velha[i][1]+velha[i][2]
-        if soma == 3 or soma == -3:
+        while self.ganhador() == 0:
+            print("Jogador", jogada % 2 + 1) #Jogada % 2 = 0 e 1 alternando + 1 = 1 e 2 alternando = jogador 1 ou 2
+            self.__tabuleiro()
+            linha = int(input("Linha: "))
+            coluna = int(input("Coluna: "))
+            # Subtrai 1 para que o usuário possa usar as posições 1, 2 e 3
+            if velha_get[linha-1][coluna-1] == 0:
+                if(jogada % 2+1) == 1:
+                    self.velha[linha-1][coluna-1] = 1
+                else:
+                    self.velha[linha-1][coluna-1] = -1
+            else:
+                print("Não está vazio")
+                jogada -= 1
+
+            if self.ganhador():
+                print("Jogador", jogada % 2 + 1, "Ganhou após", jogada+1, "rodadas")
+            jogada += 1
+
+            if jogada > 8:
+                print("Velha")
+                break
+
+
+    def ganhador(self):
+        """
+        Checa se alguma linha, coluna ou diagonal está completa, se sim, apresenta o resultado, caso não,
+        as rodadas continuam.
+        :return: Int
+        """
+        #Checar linhas
+        for i in range(3):
+            soma = self.velha[i][0]+self.velha[i][1]+self.velha[i][2]
+            if soma == 3 or soma == -3:
+                return 1
+
+        #Checar colunas
+        for i in range(3):
+            soma = self.velha[0][i] + self.velha[1][i] + self.velha[2][i]
+            if soma == 3 or soma == -3:
+                return 1
+
+        #Checar diagonais
+        __diagonal1 = self.velha[0][0]+self.velha[1][1]+self.velha[2][2] #Atributo privado
+        __diagonal2 = self.velha[0][2]+self.velha[1][1]+self.velha[2][0]
+        if __diagonal1 == 3 or __diagonal2 == -3 or __diagonal2 == 3 or __diagonal2 == -3:
             return 1
+        return 0
 
-    #Checar colunas
-    for i in range(3):
-        soma = velha[0][i] + velha[1][i] + velha[2][i]
-        if soma == 3 or soma == -3:
-            return 1
+    def __tabuleiro(self): #Metódo privado
+        """
+        Impressão do tabuleiro, o jogador 1 tem as jogadas representadas por números positivos = 1 =X
+        o jogador 2 é representado por números negativos = -1 = O.
+        :return: None
+        """
+        for i in range(3):
+            for j in range(3):
+                if self.velha[i][j] == 0:
+                    print(" _ ", end=" ") #end não permite que pule de linha automaticamente
+                elif self.velha[i][j] == 1:
+                    print(" X ", end=" ")
+                elif self.velha[i][j] == -1:
+                    print(" O ", end=" ")
+            print()
 
-    #Checar diagonais
-    diagonal1 = velha[0][0]+velha[1][1]+velha[2][2]
-    diagonal2 = velha[0][2]+velha[1][1]+velha[2][0]
-    if diagonal1 == 3 or diagonal2 == -3 or diagonal2 == 3 or diagonal2 == -3:
-        return 1
-    return 0
 
-def tabuleiro():
-    """
-    Impressão do rabuleiro, o jogador 1 tem as jogadas representadas por números positivos = 1 =X
-    o jogador 2 é representado por números negativos = -1 = O
-    :return: None
-    """
-    for i in range(3):
-        for j in range(3):
-            if velha[i][j] == 0:
-                print(" _ ", end=" ") #end não permite que pule de linha automaticamente
-            elif velha[i][j] == 1:
-                print(" X ", end=" ")
-            elif velha[i][j] == -1:
-                print(" O ", end=" ")
-        print()
+mesa = [[0, 0, 0],
+       [0, 0, 0],
+       [0, 0, 0]]
 
-menu()
+jogo_objeto = jogo_velha(mesa=mesa)
+jogo_objeto.iniciar()
